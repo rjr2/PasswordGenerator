eb239bf4eca94bec897a7a48eda85b5e9263b336
RJR <80534043+rjr2@users.noreply.github.com>
2021-06-15 11:02:44 -0400

    complete code

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # PasswordGenerator
+
+
 A password generator that can create passwords between 8 and 128 characters long.
 
-User can select capital letters, and special characters.
+User can choose to use capital letters, numbers and special characters.
+
+User can copy generated password to ClipBoard.
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,50 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var copyBtn = document.getElementById("copy");
+let generateBtn = document.querySelector("#generate");
+let inputEl = document.getElementById("input");
+let copyBtn = document.getElementById("copy");
+let upperCaseEl = document.getElementById("uppercase");
+let numbersEl = document.getElementById("numbers");
+let specialEl = document.getElementById("special");
+let errorEl = document.getElementById("error")
+let passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "01234567890";
+var symbols = "!@#$%^&*()=+-_";
 
-  passwordText.value = password;
-
+//functions
+function generatePassword(length, letters){
+  let passwordArray = letters.split("");
+  let results = [];
+  for (let i = 0; i < length; i++){
+    let index = Math.floor(Math.random()* passwordArray.length);
+    results.push(passwordArray[index]);
+  }
+  return results.join("");
 }
 
 function copyPassword(){
-  var copyText = document.querySelector("#password");
+  let copyText = document.querySelector("#password");
   copyText.select();
   document.execCommand("copy");
   alert("Saved to Clipboard");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//slider bar display
+function displayPassLength(){
+  let rangeValue = document.getElementById("input").value;
+  let display = document.getElementById("rangedisplay");
+  display.innerHTML = rangeValue
+}
+
+//event listeners
+generateBtn.addEventListener("click", function(){
+  upperCaseEl.checked ? (letters += upperCaseLetters): "";
+  numbersEl.checked ? (letters += numbers): "";
+  specialEl.checked ? (letters += symbols): "";
+  let length = inputEl.value;
+  passwordText.value = generatePassword(length, letters);
+});
+
 copyBtn.addEventListener("click", copyPassword);
